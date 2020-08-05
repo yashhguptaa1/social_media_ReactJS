368d61551ffccd575bb72252258c8f5f4b16a542
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-08-05 19:41:13 +0530

    Only files allowed to be in src folder directly

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+
+// Components
+import Navbar from './components/layout/Navbar';
+
+// Pages
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home} />
+                <Route exact path="/login" component={login} />
+                <Route exact path="/signup" component={signup} />
+              </Switch>
+            </div>
+          </Router>
+     );
+  }
+}
+ 
+export default App;
