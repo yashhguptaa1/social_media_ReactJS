05573e10200338d3161422599275a089b57ec075
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-08-06 18:37:26 +0530

    Create Nodejs Backend using Java SpringBoot

---
+++ SocialMedia/src/test/java/com/yg/SocialMedia/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yg.SocialMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
